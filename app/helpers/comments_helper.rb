47f8ea0757419e31de8a6000102a61ce4c846a22
Mary Banshee <mary.banshee@gmail.com>
2020-06-21 14:19:09 +0300

    Fix helper

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,11 +1,7 @@
 module CommentsHelper
   def nested_comments(comments)
     comments.map do |comment, sub_comments|
-      if sub_comments.present?
-        render(comment) + content_tag(:div, nested_comments(sub_comments), class: 'nested_comments')
-      else
-        render(comment)
-      end
+      render(comment) + content_tag(:div, nested_comments(sub_comments), class: 'nested_comments')
     end.join.html_safe
   end
 end
